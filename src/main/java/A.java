2bdf0dd2bed0780be619d324f1b953eb7245d172
BuMiTa <buiminhthanh2901@gmail.com>
2024-12-25 21:53:49 +0700

    List

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -6,8 +6,20 @@ public class A {
         System.out.println("What can I do for you?");
         Scanner sc= new Scanner(System.in);
         String s=sc.nextLine();
+        List<String> li= new ArrayList<>();
         while(!s.equals("bye")){
-            System.out.println(s);
+            if(!s.equals("list")){
+                li.add(s);
+                System.out.println("added: " + s);
+            }
+            else{
+                int i=1;
+                for(String str : li){
+                    String no=Integer.toString(i);
+                    System.out.println(no+". "+str);
+                    i++;
+                }
+            }
             sc= new Scanner(System.in);
             s=sc.nextLine();
         }
