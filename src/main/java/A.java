fe7b73795f8d0f1bb2cfe8cc0255d1f82026e48b
BuMiTa <buiminhthanh2901@gmail.com>
2024-12-25 10:52:33 +0700

    Rename bot

---
+++ src/main/java/A.java
@@ -0,0 +1,8 @@
+public class A {
+    public static void main(String[] args) {
+        String name="a";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    
+}
