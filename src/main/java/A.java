69198f4f4663f7d16eec1441bd3722d4dda909cf
BuMiTa <buiminhthanh2901@gmail.com>
2024-12-25 11:07:40 +0700

    Echo

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,8 +1,16 @@
+import java.util.*;
 public class A {
     public static void main(String[] args) {
         String name="a";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+        Scanner sc= new Scanner(System.in);
+        String s=sc.nextLine();
+        while(!s.equals("bye")){
+            System.out.println(s);
+            sc= new Scanner(System.in);
+            s=sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
-    
+    }
 }
